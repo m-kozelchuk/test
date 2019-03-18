7e666264372e86f3bcf72d358647dfda65be73aa
Maryna Kozelchuk <marina.kozelchuk@gmail.com>
2019-03-18 17:55:12 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# test
+Gulp Boilerplate for generic CSS/HTML/JS development
+=============
+
+Based on nunjucks, sass, awebpack
+
+Structure
+=============
+`/src/` - thats where you write code.
+
+`/build/` - compiled code. Do not ever edit this folder.
+
+
+How to use
+=============
+
+1) `$ npm install`
+
+2) run `$ gulp build` to build initial files for build (only for the first time)
+
+3) `$ gulp` - it will start local server at <a href="http://localhost:8080/">http://localhost:8080/</a>
+
+4) `$ npm run build` - it will delete your build folder first and then compile project in production mode(using code minifying etc.)
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var rimraf = require('rimraf');
+var config = require('../config');
+
+// gulp.task('watch', [
+//     'sprite:watch',
+//     'sass:watch',
+//     'copy:watch',
+//     'sprite:svg:watch',
+//     'svgo:watch',
+//     'nunjucks:watch',
+//     'webpack:watch'
+// ], function() {});
+gulp.task('watch', gulp.parallel(
+  'sprite:watch',
+  'sass:watch',
+  'copy:watch',
+  'sprite:svg:watch',
+  'svgo:watch',
+  // 'nunjucks:watch',
+  'pug:watch',
+  'webpack:watch',
+));
+
+
+gulp.task('delete', function (cb) {
+    rimraf('./'+config.dest.root, cb);
+});
+gulp.task('default', gulp.series('server', 'watch'));
+// gulp.task('build', gulp.series('nunjucks', 'sprite', 'sprite:svg', 'copy','webpack','sass:build', 'tinypng'));
+gulp.task('build', gulp.series('pug', 'sprite', 'sprite:svg', 'copy','webpack','sass:build', 'tinypng'));
